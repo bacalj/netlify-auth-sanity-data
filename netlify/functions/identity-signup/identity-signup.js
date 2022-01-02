4de8372b0d449aa0fa7c4e1f52893d95443abc5a
Joseph Bacal <bacalj@gmail.com>
2022-01-01 22:02:01 -0500

    lets see if this really works

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -1,18 +1,11 @@
-// note - this function MUST be named `identity-signup` to work
-// we do not yet offer local emulation of this functionality in Netlify Dev
-//
-// more:
-// https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
-// https://www.netlify.com/docs/functions/#identity-and-functions
-
 const handler = async function (event) {
   const data = JSON.parse(event.body)
   const { user } = data
 
   const responseBody = {
     app_metadata: {
-      roles: user.email.split('@')[1] === 'trust-this-company.com' ? ['editor'] : ['visitor'],
-      my_user_info: 'this is some user info',
+      roles: ['basic-user'],
+      my_user_info: 'can we really just create a field of sorts?',
     },
     user_metadata: {
       // append current user metadata
@@ -20,6 +13,10 @@ const handler = async function (event) {
       custom_data_from_function: 'hurray this is some extra metadata',
     },
   }
+
+  console.log("USER CREATED: ", user)
+  console.log("RESPONSEBODY: ", responseBody)
+  
   return {
     statusCode: 200,
     body: JSON.stringify(responseBody),
