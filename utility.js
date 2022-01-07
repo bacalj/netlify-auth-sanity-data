486f00da0c6feae649021dfcd82e2fc60bd5c3a8
Joseph Bacal <bacalj@gmail.com>
2022-01-07 13:04:18 -0500

    front end stuff, no need for these utilities really, cleanup

--- utility.js
+++ utility.js
@@ -1,42 +1,12 @@
-/* UTILITY */
-function niceTime(){
-    let newDate = new Date();
-    return newDate.toUTCString().substring(17,25);
-}
-
-function writeMessage(msg){ 
-    let newItem = document.createElement('li');
-    let newText = document.createTextNode(msg + ' at ' + niceTime());
-    newItem.appendChild(newText);
-    document.getElementById("messages").appendChild(newItem);
-}
-
-function theUser(){
-    return JSON.parse(localStorage.getItem('gotrue.user'))
-}
-
-function setFoo(str){
-    window.localStorage.setItem('foo', str)
-}
-
-// document.cookies.onChanged.addListener(function(changeInfo) {
-//     console.log('Cookie changed: ' +
-//                 '\n * Cookie: ' + JSON.stringify(changeInfo.cookie) +
-//                 '\n * Cause: ' + changeInfo.cause +
-//                 '\n * Removed: ' + changeInfo.removed);
-// });
-
-/*
-https://github.com/netlify/netlify-identity-widget
-
-*/
-
-window.netlifyIdentity.on('init', (u) => {
-    writeMessage('initialized netlifyIdentity at' + niceTime(), + ' user is: ', u)
+window.netlifyIdentity.on('init', () => {
+    console.log('netlifyIdentity local object ready')
 })
 
-window.netlifyIdentity.on('login', () => {
-    writeMessage('logged in the user')
+window.netlifyIdentity.on('login', (u) => {
+    console.log('logging in a user, giving them a token here')
+    console.log(u)
 })
 
-
+window.netlifyIdentity.on('logout', () => {
+    console.log('user logged out')
+})
