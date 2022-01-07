5022382fc21b11ef15c4161b516e172bf26f4db9
Joseph Bacal <bacalj@gmail.com>
2022-01-07 15:59:26 -0500

    logged in or out body class

--- utility.js
+++ utility.js
@@ -5,8 +5,15 @@ window.netlifyIdentity.on('init', () => {
 window.netlifyIdentity.on('login', (u) => {
     console.log('logging in a user, giving them a token here')
     console.log(u)
+    applyBodyClass('logged-in')
 })
 
 window.netlifyIdentity.on('logout', () => {
     console.log('user logged out')
+    applyBodyClass('logged-out')
 })
+
+applyBodyClass = (str) => {
+    document.querySelector('body').className = ''
+    document.querySelector('body').classList.add(str)
+}
