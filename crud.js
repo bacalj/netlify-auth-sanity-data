16e1cf55f50fa676b447673d5a6f6fd7a55ec925
Joseph Bacal <bacalj@gmail.com>
2022-01-08 16:58:30 -0500

    crude front end response

--- crud.js
+++ crud.js
@@ -25,6 +25,9 @@ function createUserNote(){
             }
         }).then((r) => {
             console.log(r)
+            if (!r.ok){
+                alert('you need to refresh your login, then try again.')
+            }
             // TODO handle response from netlify function, e.g. if it 500s because logout has expired
             // we should prompt user to refresh login
 
